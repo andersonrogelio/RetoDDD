7ffe48c4d4047f1e64d52e786d5a2d43a56a35c3
Anderson <andersonrogelio0@gmail.com>
2022-07-31 11:09:30 -0300

    DOMAIN EVENT: PedidoCreado

--- domain/src/main/java/co/com/sofka/atencionalcliente/pedido/events/PedidoCreado.java
+++ domain/src/main/java/co/com/sofka/atencionalcliente/pedido/events/PedidoCreado.java
@@ -5,14 +5,27 @@ import co.com.sofka.atencionalcliente.pedido.values.DescripcionPedido;
 import co.com.sofka.atencionalcliente.pedido.values.FechaEntrega;
 import co.com.sofka.domain.generic.DomainEvent;
 
-public class PedidoCreada extends DomainEvent {
-//    private final DescripcionPedido descripcionPedido;
-//    private final FechaEntrega fechaEntrega;
-//    private final Costo costo;
-//    public PersonaCreada(DescripcionPedido descripcionPedido, FechaEntrega fechaEntrega, Costo costo){
-//        super("co.com.sofka.atencionalcliente.PedidoCreado");
-//        this.descripcionPedido = descripcionPedido;
-//        this.fechaEntrega = fechaEntrega;
-//        this.costo = costo;
-//    }
+public class PedidoCreado extends DomainEvent {
+    private final DescripcionPedido descripcionPedido;
+    private final FechaEntrega fechaEntrega;
+    private final Costo costo;
+    public PedidoCreado (DescripcionPedido descripcionPedido, FechaEntrega fechaEntrega, Costo costo){
+//        super("co.com.sofka.atencionalcliente.pedido");
+        super("co.com.sofka.atencionalcliente.pedido.pedidocreado");
+        this.descripcionPedido = descripcionPedido;
+        this.fechaEntrega = fechaEntrega;
+        this.costo = costo;
+    }
+
+    public DescripcionPedido getDescripcionPedido() {
+        return descripcionPedido;
+    }
+
+    public FechaEntrega getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public Costo getCosto() {
+        return costo;
+    }
 }
