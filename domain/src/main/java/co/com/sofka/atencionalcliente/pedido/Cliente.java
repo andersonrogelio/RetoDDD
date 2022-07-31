85c3744bb9436a8612165159521325ac41ebb14a
Anderson <andersonrogelio0@gmail.com>
2022-07-31 13:21:22 -0300

    Creacion de la entidad Producto junto a sus objetos de valor

--- domain/src/main/java/co/com/sofka/atencionalcliente/pedido/Cliente.java
+++ domain/src/main/java/co/com/sofka/atencionalcliente/pedido/Cliente.java
@@ -5,15 +5,22 @@ import co.com.sofka.atencionalcliente.pedido.values.InformacionCliente;
 import co.com.sofka.atencionalcliente.pedido.values.Nombre;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class Cliente extends Entity<ClienteID> {
-    private final Nombre nombre;
-    private final InformacionCliente informacion;
+    private Nombre nombre;
+    private  InformacionCliente informacion;
     public Cliente(ClienteID entityId , Nombre nombre, InformacionCliente informacion){
         super(entityId);
         this.nombre = nombre;
         this.informacion = informacion;
     }
-
+    public  void  updateNombre(Nombre nombre){
+        this.nombre = this.nombre = Objects.requireNonNull(nombre);
+    }
+    public  void  updateInformacion(InformacionCliente informacion){
+        this.informacion = Objects.requireNonNull(informacion);
+    }
     public Nombre nombre(){
         return  nombre;
     }
--- domain/src/main/java/co/com/sofka/atencionalcliente/pedido/Producto.java
+++ domain/src/main/java/co/com/sofka/atencionalcliente/pedido/Producto.java
@@ -1,4 +1,16 @@
 package co.com.sofka.atencionalcliente.pedido;
 
-public class Producto {
+import co.com.sofka.atencionalcliente.pedido.values.DescripcionProducto;
+import co.com.sofka.atencionalcliente.pedido.values.NombreProducto;
+import co.com.sofka.atencionalcliente.pedido.values.ProductoID;
+import co.com.sofka.domain.generic.Entity;
+
+public class Producto extends Entity<ProductoID> {
+    private NombreProducto nombre;
+    private DescripcionProducto descripcion;
+    public Producto(ProductoID entityId, NombreProducto nombre, DescripcionProducto descripcion) {
+        super(entityId);
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
 }
